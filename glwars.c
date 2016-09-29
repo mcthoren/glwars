c06dc4176a87e7fcc1bb8ecd9fea109b541f15c1
mcthoren <thoren.mcdole@gmail.com>
2016-09-29 14:00:46 +0000

    reorder headers per style(9)

--- glwars.c
+++ glwars.c
@@ -1,16 +1,16 @@
-#include <GL/glut.h>
-#include <stdlib.h>
-#include <math.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
+#include <netdb.h>
 #include <arpa/inet.h>
-#include <unistd.h>
+#include <netinet/in.h>
+
+#include <GL/glut.h>
+#include <math.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <string.h>
-#include <netdb.h>
-
+#include <stdlib.h>
+#include <unistd.h>
 
 #include "glwars.h"
 
